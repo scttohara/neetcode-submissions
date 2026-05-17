75c75c30614e379403cb5bfcb6398407d1770873
Scott O'Hara <scttohara15@gmail.com>
2026-05-17 13:01:31 -0400

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        from collections import defaultdict
+        
+        if len(strs) <= 1:
+            return [strs]
+        
+        list_to_return = defaultdict(list)
+        for current_string in strs:
+
+            current_string_count = [0] * 26
+            for char in current_string:
+                current_string_count[ord(char) - ord('a')] += 1
+
+            list_to_return[tuple(current_string_count)].append(current_string)
+            
+        return list(list_to_return.values())
