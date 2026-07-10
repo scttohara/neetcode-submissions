51d782fec08c22df4a15583bd8276825c4bb243a
Scott O'Hara <scttohara15@gmail.com>
2026-07-10 09:54:35 -0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,24 @@
+class Solution:
+	def isAnagram(self, s: str, t: str) -> bool:
+        
+		if len(s) != len(t):
+			return False
+
+		s_dict = {}
+		for char in s:
+			if char in s_dict:
+				s_dict[char] += 1
+			else:
+				s_dict[char] = 1
+		
+		t_dict = {}
+		for char in t:
+			if char in t_dict:
+				t_dict[char] += 1
+			else:
+				t_dict[char] = 1
+
+		if s_dict == t_dict:
+			return True
+	
+		return False
